38d78ef1fc990d9cf98ac3d8bd7805c4b858c77b
Ashwin Bordoloi <Ashwin.Bordoloi@INEIGHT.COM>
2017-06-05 18:03:34 +0530

    First Commit

---
+++ js/create-graph.js
@@ -0,0 +1,117 @@
+function parseData(createGraph) {
+    Papa.parse("../data.csv", {
+        download: true,
+        complete: function(results) {
+            console.log(results.data);
+            createGraph(results.data);
+        }
+    });
+}
+
+function createGraph(data) {
+    // var product = [];
+    // var product1 = [];
+    // var product2 = [];
+    // var product3 = [];
+    // var product4 = [];
+
+    var priLst = [];
+    var prdNmLst = [];
+
+    var chrtColumns = [];
+    var chrtClmn = [];
+    var cellValue = 0;
+    if (data != undefined && data.length) {
+
+        /*Exclude  first cell value and get product name*/
+        for (var i = 1; i < data.length; i++) {
+            if (data[i][0] != undefined) {
+                prdNmLst.push(data[i][0]);
+            }
+        }
+        /*Exclude first cell value and fetch bug priority list*/
+        for (var i = 1; i < data[0].length; i++) {
+            if (data[0][i]) {
+                priLst.push({ prdNm: data[0][i], prdIndx: i });
+            }
+        }
+
+        for (var pIndx = 0; pIndx < priLst.length; pIndx++) {
+            var prdItem = priLst[pIndx];
+            chrtClmn = [];
+            for (var i = 0; i < data.length; i++) {
+
+                cellValue = data[i][prdItem.prdIndx];
+                if (cellValue != undefined) {
+                    chrtClmn.push(cellValue == "" ? 0 : cellValue);
+                }
+            }
+            chrtColumns.push(chrtClmn);
+
+        }
+
+    }
+    // for (var i = 0; i < data.length; i++) {
+    //     if (data[i][1])
+    //         product1.push(data[i][1]);
+    // }
+
+
+    // for (var i = 0; i < data.length; i++) {
+    //     if (data[i][2])
+    //         product2.push(data[i][2]);
+    // }
+
+    // for (var i = 0; i < data.length; i++) {
+    //     if (data[i][3])
+    //         product3.push(data[i][3]);
+    // }
+    // for (var i = 0; i < data.length; i++) {
+    //     if (data[i][4])
+    //         product4.push(data[i][4]);
+    // }
+
+    // var columns = [];
+
+    // columns.push(product1);
+    // columns.push(product2);
+    // columns.push(product3);
+    // columns.push(product4);
+
+
+
+    var chart = c3.generate({
+
+        bindto: '#chart',
+        data: {
+            columns: chrtColumns
+
+        },
+        axis: {
+
+            x: {
+                type: 'category',
+                categories: prdNmLst
+            }
+        },
+        zoom: {
+            enabled: true
+        },
+        legend: {
+            position: 'right'
+        },
+
+        grid: {
+            x: {
+                show: true
+            },
+            y: {
+                show: true
+            }
+        }
+
+
+    });
+}
+
+parseData(createGraph);
---
+++ js/script.js
@@ -0,0 +1,40 @@
+function greet(greeting) {
+    console.log(greeting);
+    $('#dat').text(greeting);
+}
+
+function getRandom(arr) {
+    var rand = Math.random();
+    return arr[Math.floor(rand * arr.length)];
+}
+
+var greetings = [
+    "Error1",
+    "Error2",
+    "Error3",
+    "Error4"
+];
+
+/*---------------------------*/
+
+
+$(document).ready(function() {
+    ion.sound({
+        sounds: [
+
+            { name: "glass" }
+        ],
+
+        path: "ring/",
+        preload: false,
+        volume: 10.0
+    });
+    setInterval(function() {
+        ion.sound.play("glass");
+        greet(getRandom(greetings));
+        $(".alert").fadeIn("fast").fadeOut(5000, function() {
+            $('#try').append('<li class="list-group-item">' + $('#dat').html() + '</li>');
+        });
+    }, 8000);
+
+});
